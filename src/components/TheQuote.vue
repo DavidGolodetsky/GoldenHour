5d0cf8a74a3ec4252ae652d64f30788d22de8c49
DavidGo <davidgolodetsky@gmail.com>
2020-10-30 23:06:54 +0200

    first commit

---
+++ src/components/TheQuote.vue
@@ -0,0 +1,26 @@
+<template>
+  <blockquote v-if="quote">
+    <p>{{ quote.text }}</p>
+    <footer> — {{ quote.author }}</footer>
+  </blockquote>
+</template>
+
+<script>
+import quotes from '../data/quotes.json'
+import { ref } from 'vue'
+
+export default {
+  setup () {
+
+    let quote = ref(null)
+
+    const getQuote = (() => {
+      const quotesArr = JSON.parse(JSON.stringify(quotes))
+      quote = quotesArr[Math.floor(Math.random() * quotesArr.length)]
+    })()
+
+    return { quote }
+  },
+}
+</script>
+
---
+++ src/pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <p class="text-4xl">
+      <Icon class="iconify inline-block" icon="carbon:campsite" />
+    </p>
+    <p>
+      Golden Hour
+    </p>
+    <div class="py-4" />
+    <TheQuote />
+    <div class="py-4" />
+
+    <!-- <input
+      v-model="name"
+      :placeholder="t('intro.whats-your-name')"
+      class="px-4 py-2 border border-gray-200 rounded text-center text-sm outline-none active:outline-none bg-transparent dark:border-gray-700"
+      style="width: 250px"
+      @keydown.enter="go"
+    >
+
+    <div>
+      <button
+        class="btn m-3 text-sm"
+        :disabled="!name"
+        @click="go"
+      >
+        {{ t('button.go') }}
+      </button>
+    </div> -->
+
+    <SunPosition />
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref } from 'vue'
+// import { useRouter } from 'vue-router'
+// import { useI18n } from 'vue-i18n'
+
+// export const name = ref('')
+
+// const router = useRouter()
+// export const go = () => {
+//   if (name.value)
+//     router.push(`/hi/${name.value}`)
+// }
+
+// const { t } = useI18n()
+// export { t }
+</script>
